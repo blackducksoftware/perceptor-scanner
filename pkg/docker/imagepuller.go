711dbfad5c6c8d5d604b30e5b208fac55b63ec0e
Matt Fenwick <mfenwick100@gmail.com>
2018-03-07 09:19:08 -0500

    log header value

--- pkg/docker/imagepuller.go
+++ pkg/docker/imagepuller.go
@@ -103,7 +103,9 @@ func (ip *ImagePuller) CreateImageInLocalDocker(image Image) error {
 
 	// TODO if the image *isn't* from the local registry, then don't do this header stuff
 
-	req.Header.Set("X-Registry-Auth", encodeAuthHeader(ip.dockerUser, ip.dockerPassword))
+	headerValue := encodeAuthHeader(ip.dockerUser, ip.dockerPassword)
+	log.Infof("X-Registry-Auth value:\n%\n", headerValue)
+	req.Header.Set("X-Registry-Auth", headerValue)
 
 	resp, err := ip.client.Do(req)
 	if err != nil {
