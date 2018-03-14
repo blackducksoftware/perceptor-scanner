0a7775d1a35344d7774d509568900ed1baacfc96
Matt Fenwick <mfenwick100@gmail.com>
2018-03-14 11:26:06 -0400

    remove obsoleted code

--- cmd/imagefacadewebserver/run.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-oc create ns bds-perceptor
-oc create -f if-serviceaccount.yaml --namespace=bds-perceptor
-# allows launching of privileged containers for Docker machine access
-oc adm policy add-scc-to-user privileged system:serviceaccount:bds-perceptor:if-sa
-oc create -f if.yaml --namespace=bds-perceptor
-oc create -f route.yaml --namespace=bds-perceptor
