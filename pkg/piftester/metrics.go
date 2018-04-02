d0ae99acdca318fd24b6048b56a11cce241cbdcc
Matt Fenwick <mfenwick100@gmail.com>
2018-04-02 16:17:07 -0400

    add piftester metric, unit test

---
+++ pkg/piftester/metrics.go
@@ -0,0 +1,46 @@
+/*
+Copyright (C) 2018 Synopsys, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements. See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership. The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package piftester
+
+import (
+	"fmt"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var imagePullCounter *prometheus.CounterVec
+
+func recordImagePullResult(success bool) {
+	successString := fmt.Sprintf("%t", success)
+	imagePullCounter.With(prometheus.Labels{"success": successString}).Inc()
+}
+
+func init() {
+	imagePullCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "perceptor",
+		Subsystem: "piftester",
+		Name:      "image_pull",
+		Help:      "success/failure of imagefacade pulls as communicated to piftester",
+	},
+		[]string{"success"})
+	prometheus.MustRegister(imagePullCounter)
+}
